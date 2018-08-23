6d40b6d59f4102210093c5566ea32df5147f1fd2
Marco G. Maia <marcogmaia@gmail.com>
2018-08-23 06:19:51 -0300

    Add files via upload

--- custom.lua
+++ custom.lua
@@ -1,51 +1,8 @@
-autopickup_exceptions += >uselessness, >inaccuracy
-confirm_butcher = never
-auto_butcher = very full
-chunks_autopickup = true
-easy_eat_chunks = true
-easy_eat_gourmand = true
-auto_eat_chunks = true
-# easy_eat_contaminated = false
-runrest_stop_message += corpse
-message_colour += mute:You (start|continue|finish|stop) butchering
-message_colour += mute:This raw flesh tastes
-message_colour += mute:You continue eating
-
-autoinscribe += (inedible|mutagenic|forbidden).*(chunk|corpse):noeat
-autofight_stop = 75
-hp_warning = 50
-
-rest_wait_percent = 99
-explore_auto_rest = true
-
-travel_delay = 1
-explore_delay = 1
-rest_delay = -1
-show_travel_trail = true
-# explore_stop  = items,stairs,shops,altars,portals,branches,runed_doors
-# explore_stop += greedy_visited_item_stack
-auto_exclude = oklob plant,statue,curse skull,roxanne
-runrest_ignore_poison  = 2:30
-runrest_ignore_monster = butterfly:1
-trap_prompt = false
-trapwalk_safe_hp = dart:1,needle:1,arrow:1,bolt:1,spear:1,axe:1,blade:1,teleport:1,zot:1,alarm:1
-
 {
 LOS = 7
 
 function at_feet()
   return iter.invent_iterator:new(you.floor_items())
-end
-
-function count_nearby(cx, cy, r)
-  local x, y
-  local i = 0
-  for x = -r, r do
-    for y = -r, r do
-      i = i+1
-
-    end
-  end
 end
 
 need_skills_opened = true
@@ -56,7 +13,6 @@ function open_skills()
   end
 end
 
-
 function Set(list)
   local set = {}
   for _, l in ipairs(list) do
@@ -64,13 +20,11 @@ function Set(list)
   end
 end
 
-
-function check_dun()
+function check_dungeon()
   -- check if you are in a valid level for auto_hunting
   local dungeon = {"D:1", "D:2", "D:3", "D:4", "D:5", "D:6", "D:7", "D:8", "D:9"}
   for _, valid_level in pairs(dungeon) do
     if you.where() == valid_level then
-      -- crawl.mpr("you are in " .. you.where())
       return true
     end
   end
@@ -82,25 +36,73 @@ function count_nearby(cx, cy, r)
   local i = 0
   for x = -r, r do
     for y = -r, r do
-
+      if max_dist(x, y) > 0 and monster.get_monster_at(x, y) then
+        i = i+1
+      end
+    end
+  end
+  return i
 end
 
-function max(a, b)
-  return (a < b) ? b : a;
+function max_dist(x, y)
+  return math.max(math.abs( x ), math.abs( y ))
 end
+
+-- function delta_to_vi(dx, dy)
+--   local d2v = {
+--     [-1] = {[-1] = 'y', [0] = 'h', [1] = 'b'},
+--     [0]  = {[-1] = 'k', [0] = 's', [1] = 'j'},
+--     [1]  = {[-1] = 'u', [0] = 'l', [1] = 'n'},
+--   }
+--   return d2v[dx][dy]
+-- end
 
 function do_hunt()
-  if check_dun() then
-
+  if check_dungeon() then
+    monsters_in_LOS = count_nearby(0, 0, LOS)
+    monsters_around = count_nearby(0, 0, 1)
+    if monsters_in_LOS > 0 then
+      crawl.mpr("Monsters in sight = " .. monsters_in_LOS)
+    end
+    if monsters_around >= 2 and not you.berserk() then
+      crawl.sendkeys("aa")
+    end
+    if (you.status("-Berserk") or you.status("Berserk")) and monsters_in_LOS == 0 then
+      crawl.sendkeys('5')
+    end
   end
 end
 
-function ready()
-  -- if you.xl() < 11 and
-  -- if you.xl() < 11 and you.where() ~= "Dungeon"
-
-  open_skills()
+function you_status(str)
+  if string.find(you.status(), str) then
+    return true
+  end
+  return false
 end
 
-}
+function rest_status()
+  status_list = {"berserk", "cooldown", "pois", "slow"}
+  for _, val in ipairs(status_list) do
+    if you_status(val) then
+      return true
+    end
+  end
+  return false
+end
 
+function rest_berserk()
+  monsters_in_LOS = count_nearby(0, 0, LOS)
+  if monsters_in_LOS == 0 and (rest_status()) then
+    crawl.sendkeys("5")
+  end
+  -- crawl.mpr(you.status())
+end
+
+function ready()
+  open_skills()
+  if check_dungeon() then
+    rest_berserk()
+  end
+  -- do_hunt()
+end
+}
